28f91d10a01918d01d2e9d0beec67b79b7d0872c
zhaozhang <zhaozhang@prev.cs.uchicago.edu>
2013-09-18 16:24:49 -0500

    adding launch script

---
+++ bin/launch-amfora.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+### launch-amfora.sh will launch amfora on one host
+### in background, pid is stored in file ./pid
+### 
+### 
+
+if [ $# -lt 4 ]; then
+    echo "wrong format"
+    echo "format: ${0} </mount/point> <amfora.conf> <hostip> <AMFORA_HOME>"
+    echo "example: ${0} /tmp/amfora etc/amfora.conf 192.168.0.1 ${AMFORA_HOME}"
+    exit
+fi
+
+
+mount=${1}
+file=${2}
+ip=${3}
+
+if [ "${AMFORA_HOME}" = "" ];then
+    echo "Environment variable AMFORA_HOME is not defined"
+    exit
+fi
+
+pwd=`pwd`
+cd ${AMFORA_HOME}
+src/amfora.py ${mount} ${file} ${hostip} >> /tmp/amfora.log 2>&1 &
+echo $! > pid
+
+cd $pwd
+
