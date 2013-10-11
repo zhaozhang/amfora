862696da223b51ffa235474aa444a7beb6604b67
zhaozhang <zhaozhang@bigben2.cs.uchicago.edu>
2013-10-11 14:41:18 -0500

    bug fix

--- examples/grep/grep.sh
+++ examples/grep/grep.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-cp -r example/grep/data /tmp/amfora
+cp -r examples/grep/data /tmp/amfora
 amc.py scatter /tmp/amfora/data
 
-touch /tmp/amfora/temp
+mkdir /tmp/amfora/temp
 for file in `ls /tmp/amfora/data`
 do
-    amc.py queue grep hello /tmp/amfora/data/${file} > /tmp/amfora/temp/${file}
-done
-amc.py execute
+    src/amc.py queue "grep granted /tmp/amfora/data/${file} > /tmp/amfora/temp/${file}
+done"
+src/amc.py execute
 
-cat /tmp/amfora/temp/* > /tmp/amfora/result.txt
-
-cp /tmp/amfora/result.txt ./result.txt
+cat /tmp/amfora/temp/* 
