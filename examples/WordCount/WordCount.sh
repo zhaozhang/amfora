d85711c73d711d4eb1742acccd849938b727dce7
zhaozhang <zhaozhang@bigben2.cs.uchicago.edu>
2013-10-11 14:37:20 -0500

    bug fix

--- examples/WordCount/WordCount.sh
+++ examples/WordCount/WordCount.sh
@@ -8,5 +8,6 @@ for file in `ls /tmp/amfora/data`
 do
    src/amc.py queue "wc -w /tmp/amfora/data/${file} | -d ' ' -f 1> /tmp/amfora/temp/${file}"
 done
+src/amc.py execute
 
 cat /tmp/amfora/temp/* | awk '{sum+=$1} END{print sum}'
