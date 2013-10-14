c511f430796fbfcff87b1df1750bf2627c4fecab
zhaozhang <zhaozhang@bigben2.cs.uchicago.edu>
2013-10-14 17:08:18 -0500

    adding iterative pagerank

---
+++ examples/PageRank/IterPageRank.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+#This is simplied version of PageRank. This example source code did not apply the dumping factor of 0.85 to the results. This example source code assumes all pages are cited in other pages.
+
+cp -r examples/PageRank/data /tmp/amfora/
+src/amc.py scatter /tmp/amfora/data
+cp examples/PageRank/score.txt /tmp/amfora/score-0.txt
+
+ID=0
+Converge=1
+
+while [ ${Converge} == 1 ]
+do
+    src/amc.py multicast /tmp/amfora/score-${ID}.txt
+
+    mkdir /tmp/amfora/temp-${ID}
+    for file in `ls /tmp/amfora/data`
+    do
+	src/amc.py queue "examples/PageRank/bin/distribution.py /tmp/amfora/data/${file} /tmp/amfora/data/score.txt > /tmp/amfora/temp-${ID}/${file}"
+    done
+    src/amc.py execute
+
+    mkdir /tmp/amfora/target-${ID}
+    src/amc.py shuffle /tmp/amfora/temp-${ID} /tmp/amfora/target-${ID}
+
+    mkdir /tmp/amfora/result-${ID}
+    for file in `ls /tmp/amfora/target-${ID}`
+    do
+	src/amc.py queue "examples/PageRank/bin/addition.py /tmp/amfora/target-${ID}/${file} > /tmp/amfora/result-${ID}/${file}"
+    done
+    src/amc.py execute
+
+    cat /tmp/amfora/result-${ID}/* | sort -nk 1 > /tmp/amfora/score-${ID}.txt
+    
+    if [ ${ID} -gt 0 ]; then
+	prevID=`expr ${ID} - 1`
+	Converge=`diff /tmp/amfora/score-${ID}.txt /tmp/amfora/score-${prevID}.txt | echo $?`
+
+    ID=`expr ${ID} + 1`	
+done
