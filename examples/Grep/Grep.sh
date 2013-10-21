cd05153ee037e19df82eb2e949add518186331d2
zhaozhang <zhaozhang@bigben2.cs.uchicago.edu>
2013-10-21 12:51:09 -0500

    fixed a minor bug

--- examples/Grep/Grep.sh
+++ examples/Grep/Grep.sh
@@ -5,8 +5,8 @@ amc.py scatter /tmp/amfora/data
 mkdir /tmp/amfora/temp
 for file in `ls /tmp/amfora/data`
 do
-    src/amc.py queue "grep granted /tmp/amfora/data/${file} > /tmp/amfora/temp/${file}
-done"
+    src/amc.py queue "grep granted /tmp/amfora/data/${file} > /tmp/amfora/temp/${file}"
+done
 src/amc.py execute
 
 cat /tmp/amfora/temp/* > /tmp/amfora/result.txt
