731b067909233a43af7f9cc283f17ef5cecac9b1
zhaozhang <zhaozhang@bigben2.cs.uchicago.edu>
2013-11-19 09:02:36 -0600

    turn on log

--- src/amfora.py
+++ src/amfora.py
@@ -34,9 +34,9 @@ class Logger():
         self.fd = open(logfile, "w")
 
     def log(self, info, function, message):
-        #self.fd.write("%s: %s %s %s\n" % (str(datetime.datetime.now()), info, function, message))
-        #self.fd.flush()
-        print("%s: %s %s %s" % (str(datetime.datetime.now()), info, function, message))
+        self.fd.write("%s: %s %s %s\n" % (str(datetime.datetime.now()), info, function, message))
+        self.fd.flush()
+        #print("%s: %s %s %s" % (str(datetime.datetime.now()), info, function, message))
 
 if not hasattr(__builtins__, 'bytes'):
     bytes = str
